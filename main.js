82eb74f50d6c9a03618ebc3f15eca5103e040572
AllenBer <allenbernardo655@gmail.com>
2025-06-10 12:56:55 -0600

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('btnGeneral').addEventListener('click', () => {
-  window.location.href = 'documentacion-general.html';
-});
-
-document.getElementById('btnEmpresa').addEventListener('click', () => {
-  window.location.href = 'documentacion-empresa.html';
-});
